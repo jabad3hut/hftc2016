ade96af0eb13d17a7fed49b1bd75d217b1b2d429
Nathan Johnson <njohnson@ena.com>
2016-10-14 14:27:05 -0500

    forms submit

--- html/request.js
+++ html/request.js
@@ -4,25 +4,44 @@ $(document).ready(function(){
             "url": "mockproduct.json",
             "dataSrc": ""
         },
-        "columns": [
-            { "data": "productName" },
-            { "data": "productDesc" },
-            { "data": "location" },
-            { "render":
+        "columnDefs": [
+            { "targets": "_all", "className": "table-cell" },
+            { "targets": 0, "data": "productName" },
+            { "targets": 1, "data": "productDesc" },
+            { "targets": 2, "data": "location" },
+            { "targets": 3, "render":
               function(data, type, row, meta) {
                 return row.totalInventory - row.reservedInventory;
               }
             },
-            { "render": function(data, type, row, meta) {
-
-                  return '<input name="itemid" class="quantity-input" id="' + row.id + '">';
+            { "targets": 4, "render":
+              function(data, type, row, meta) {
+                  
+                  return '<input type="text" name="quantity" id="qty'+row.id+'" />';
               }
             },
-            { "render": function(data, type, row, meta) {
-
-                  return '<input type="hidden" name="itemid"><a href="/whatever?id=' + row.id + '">Request</a>';
+            { "targets": 5, "render":
+              function(data, type, row, meta) {
+                  return '<a onclick="submitRequest('+row.id+');">Request</a>';
               }
             }
         ]
     } );
 });
+
+function submitRequest(id)  {
+    var qty = $('#qty'+id).val();
+    var familyId = $('#request-items .family-number-input').val();
+    userId = $('#request-items input[name=userId]').val();
+    var payload = {
+        "userId": userId,
+        "familyId": familyId,
+        "qty": qty,
+        "itemid": id
+    };
+    var $form = $('<form method="POST" action="/inventorymanagement/submitForRequestApproval"></form>');
+    for (var key in payload) {
+        $('<input>').attr('type','hidden').attr('name',key).attr('value',payload[key]).appendTo($form);
+    }
+    $form.submit();
+}
