c247ac66e4708a5d8e46239246bcd5ff3231f975
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-18 20:48:35 -0500

    junit test working

--- inventorymanagement/src/test/java/org/pcat/inventory/test/SLF4JTest.java
+++ inventorymanagement/src/test/java/org/pcat/inventory/test/SLF4JTest.java
@@ -3,15 +3,18 @@ package org.pcat.inventory.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SLF4JTestor {
-	private static final Logger logger = LoggerFactory.getLogger(SLF4JTestor.class);
-	public static void main(String[] args) {
+import junit.framework.TestCase;
+
+public class SLF4JTest extends TestCase {
+	private static final Logger logger = LoggerFactory.getLogger(SLF4JTest.class);
+
+	public void testLogging() {
 		logger.trace("I am logging");
 		String name = "Abhijit";
 		logger.debug("Hi, {}", name);
 		logger.info("Welcome to the example of Logback");
 		logger.warn("warning message");
 		logger.error("error message");
-	}
 
+	}
 }
