207e2a19a9956b68c59538b7af1048c264544c7c
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-20 12:47:20 -0500

    The user repository is working!  Added the correct entity enablizations into the serviceContext.xml.  Set the right version for the hibernate driver to 5

--- inventorymanagement/src/test/java/org/pcat/inventory/dao/UserRepositoryTest.java
+++ inventorymanagement/src/test/java/org/pcat/inventory/dao/UserRepositoryTest.java
@@ -1,16 +1,11 @@
 package org.pcat.inventory.dao;
 
-import java.util.Properties;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -18,20 +13,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ContextConfiguration(locations = { "classpath:serviceContext.xml" })
 public class UserRepositoryTest {
 	public static final Logger logger = LoggerFactory.getLogger(UserRepositoryTest.class);
-//	@Autowired
-	UserRepository userRepository;
 	@Autowired
-	ApplicationContext ctx;
+	UserRepository userRepository;
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@Test
 	public void userAllSelectTest() {
-//		
-//		userRepo.findAll().forEach(user -> logger.info(user.toString()));
-//		userRepo.findAll().forEach(user -> logger.info(user.toString()));
-//		userRepository.findAll().forEach(user -> logger.info(user.toString()));
+		userRepository.findAll().forEach(user -> logger.info(user.toString()));
 	}
 
 }
